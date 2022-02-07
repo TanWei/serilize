a7edf4c05d67f97e88c84177015ff00ad6f4c180
lei89 <dcsvgwudi@gmail.com>
2022-02-07 11:50:04 +0800

    1、初始化仓库2、实现any

---
+++ Any.h
@@ -0,0 +1,83 @@
+#ifndef Any_H__
+#define Any_H__
+
+#include <memory>
+#include <typeindex>
+#include <functional>
+
+using namespace std;
+
+struct EXPORT_EVENTBUS Any
+{
+	Any(void) : m_tpIndex(std::type_index(typeid(void))){}
+	Any(Any& that) : m_ptr(that.Clone()), m_tpIndex(that.m_tpIndex) {}
+	Any(Any&& that) : m_ptr(move(that.m_ptr)), m_tpIndex(that.m_tpIndex) {}
+
+	template<typename U,
+		class = typename std::enable_if<!is_same<typename std::decay<U>::type,Any>::value,U>::type>
+	Any(U && value) : m_ptr(new Derived<typename decay<U>::type>(forward<U>(value))), m_tpIndex(type_index(typeid(typename decay<U>::type))) {}
+
+	bool IsNull() const { return !bool(m_ptr); }
+
+	template < class U > bool Is() const
+	{
+		return m_tpIndex == type_index(typeid(U));
+	}
+
+	template<class U >
+	U& AnyCast()
+	{
+		if (!Is<U>())
+		{
+            qDebug() << " can  not cast " << typeid(U).name() << " to " << m_tpIndex.name() << endl;
+			throw bad_cast();
+		}
+		auto derived = dynamic_cast<Derived<U>*> (m_ptr.get());
+		return derived->m_value;
+	}
+
+	Any& operator=(const Any & a)
+	{
+		if (m_ptr == a.m_ptr)
+			return *this;
+		m_ptr = a.Clone();
+		m_tpIndex = a.m_tpIndex;
+		return *this;
+	}
+
+private:
+	struct Base;
+	typedef unique_ptr<Base> BasePtr;
+
+	struct Base
+	{
+		virtual ~Base() {}
+		virtual BasePtr Clone() const = 0;
+	};
+
+	template <typename T>
+	struct Derived :Base 
+	{
+		template<typename U>
+		Derived(U && value) : m_value(forward<U>(value)) {}
+		BasePtr Clone() const
+		{
+			return BasePtr(new Derived<T>(m_value));
+		}
+
+
+		T m_value;
+	};
+
+	BasePtr Clone() const
+	{
+		if (m_ptr != nullptr)
+			return m_ptr->Clone();
+		return nullptr;
+	}
+
+	BasePtr m_ptr;
+	type_index m_tpIndex;
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <map>
+#include <Any.h>
+
+struct A
+{
+    int m_a;
+};
+
+struct B : public A
+{
+    vector<int> m_v1;
+    map<string, Any> m_s2any;
+};
